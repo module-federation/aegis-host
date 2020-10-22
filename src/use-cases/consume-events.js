20c09252ebce434bad39d1f475728852e4dab36a
tmidboe <tmidboe@us.ibm.com>
2020-10-22 08:12:29 -0500

    fix event consumer

--- src/use-cases/consume-events.js
+++ src/use-cases/consume-events.js
@@ -1,17 +1,27 @@
 'use strict'
 
-import InterfaceAdapter from '../lib/adapter'
+/**
+ * @typedef EventSubscriber
+ * @property {subscriptionCallback} subscribe
+ */
 
 /**
- * @typedef EventSubscription
- * @property {function(string,string):any} subscribe
+ * @callback eventHandler
+ * @param {string} topic
+ * @param {{value: string}} message
+ */
+
+/**
+ * @callback subscriptionCallback
+ * @param {string} topic
+ * @param {eventHandler} eventHandler
  */
 
 /**
  * @callback EventSource
  * @param {*} client 
- * @param {function(string,string):any} callback
- * @returns {EventSubscription} 
+ * @param {subscriptionCallback} callback
+ * @returns {EventSubscriber} 
  */
 
 const subscriptions = new Map();
@@ -21,30 +31,28 @@ const subscriptions = new Map();
  */
 export const EventSource = function (client, callback) {
   return {
-    subscribe(topic, filter) {
-      return callback(topic, filter);
+    subscribe(topic, eventHandler) {
+      return callback(topic, eventHandler);
     }
   }
 }
+
 /**
- * @typedef Consume
- * @property {function(InterfaceAdapter):any} subscribe 
+ * @typedef Consumer
+ * @property {Function} getSubscriptions
+ * @property {Function} unsubscribe
  */
 
 /**
  * 
- * @param {EventSource} eventSource
- * @returns {Consume}
+ * @returns {Consumer}
  */
-const EventConsumer = function (eventSource) {
+const EventConsumer = function (topic, id) {
   return {
-    subscribe(adapter) {
-      adapter.invoke(eventSource);
-    },
     getSubscriptions() {
       return [...subscriptions.entries()];
     },
-    unsubscribe(topic, id) {
+    unsubscribe() {
       subscriptions.get(topic).delete(id);
     }
   }
@@ -52,25 +60,23 @@ const EventConsumer = function (eventSource) {
 
 /**
  * 
- * @param {EventSource} eventSource 
+ * @param {EventSubscriber} eventSource 
  * @returns {function(*,function(EventSource))):any}
  */
 export default function consumerFactory(eventSource) {
   return async function consumeEvents(topic, id, callback) {
+    const consumer = EventConsumer(topic, id);
     if (subscriptions.has(topic)) {
       subscriptions.get(topic).set(id, callback);
-      return;
+      return consumer;
     }
-    subscriptions.set(topic, new Map());
-    subscriptions.get(topic).set(id, callback);
-    const serviceAdapter = InterfaceAdapter(topic);
-    serviceAdapter.add(eventSource, function (eventSource) {
-      eventSource.subscribe(topic, function (eventData) {
-        subscriptions.get(topic).forEach(s => s.callback(eventData));
+    subscriptions.set(topic, new Map().set(id, callback));
+    eventSource.subscribe(topic, function (eventData) {
+      subscriptions.get(topic).forEach(function (callback, id) {
+        const message = { ...eventData, consumer: EventConsumer(topic, id) };
+        callback(message);
       });
     });
-    const consumer = EventConsumer(eventSource);
-    consumer.subscribe(serviceAdapter);
     return consumer;
   }
 }
