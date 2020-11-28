40d1ef68061554ee95986b69e715427243064d1b
tmidboe <tmidboe@us.ibm.com>
2020-11-28 14:38:04 -0600

    event-based saga choreography

--- webpack/remote-entries.js
+++ webpack/remote-entries.js
@@ -40,16 +40,16 @@ module.exports = [
       return services;
     }
   },
-  {
-    name: 'shippingService',
-    url: 'http://localhost:8060/remoteEntry.js',
-    path: __dirname,
-    type: 'service',
-    importRemote: async () => {
-      const services = await import('orderService/shippingService');
-      return services;
-    }
-  },
+  // {
+  //   name: 'shippingService',
+  //   url: 'http://localhost:8060/remoteEntry.js',
+  //   path: __dirname,
+  //   type: 'service',
+  //   importRemote: async () => {
+  //     const services = await import('orderService/shippingService');
+  //     return services;
+  //   }
+  // },
   {
     name: 'addressService',
     url: 'http://localhost:8060/remoteEntry.js',
@@ -60,14 +60,14 @@ module.exports = [
       return services;
     }
   },
-  {
-    name: 'inventoryService',
-    url: 'http://localhost:8060/remoteEntry.js',
-    path: __dirname,
-    type: 'service',
-    importRemote: async () => {
-      const services = await import('orderService/inventoryService');
-      return services;
-    }
-  }
+  // {
+  //   name: 'inventoryService',
+  //   url: 'http://localhost:8060/remoteEntry.js',
+  //   path: __dirname,
+  //   type: 'service',
+  //   importRemote: async () => {
+  //     const services = await import('orderService/inventoryService');
+  //     return services;
+  //   }
+  // }
 ]
