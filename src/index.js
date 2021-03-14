ea17530682edf18e8c73549b9c1b5b46160aa399
tysonrm <trmidboe@gmail.com>
2021-03-13 23:08:50 -0600

    docs

--- src/index.js
+++ src/index.js
@@ -6,6 +6,13 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 8070;
 
+/**
+ * Load federated server module. Call `clear` to delete non-webpack cache if 
+ * hot reloading. Call `start` to import remote models, adapters, services, 
+ * set API routes and load persisted data from storage.
+ * @param {express} app express server
+ * @param {boolean} hot `true` if hot reloading, `false` by default
+ */
 async function startMicroLib(app, hot = false) {
   const remoteEntry = importFresh("./remoteEntry");
   const factory = await remoteEntry.microlib.get("./server");
@@ -16,12 +23,20 @@ async function startMicroLib(app, hot = false) {
   serverModule.default.start(app);
 }
 
+/**
+ * Callbacks attached to existing routes are stale.
+ * Clear API routes.
+ * @param {express} app express server
+ */
 function clearExpressRoutes(app) {
   app._router.stack = app._router.stack.filter(
     k => !(k?.route?.path && k.route.path.startsWith("/api"))
   );
 }
 
+/**
+ * Initial startup
+ */
 startMicroLib(app).then(() => {
   app.use(express.json());
   app.use(express.static("public"));
@@ -30,6 +45,12 @@ startMicroLib(app).then(() => {
   });
 });
 
+/**
+ * Trigger a hot reload:
+ * clear routes,
+ * reimport server & remotes
+ * clear non-webpack cache.
+ */
 app.get("/reload", async (req, res) => {
   try {
     clearExpressRoutes(app);
