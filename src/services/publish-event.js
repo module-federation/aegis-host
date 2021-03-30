ef27068dc92782b3d70b90435e9bbc32ef31b859
tyson <tyson7757@comcast.net>
2021-03-29 22:55:40 -0500

    refactor and cleanup

--- src/services/publish-event.js
+++ src/services/publish-event.js
@@ -1,10 +1,8 @@
-import log from "../lib/logger";
-
 export default async (event, observer) => {
   try {
     const publishEvent = (await import("orderService/services")).publishEvent;
     publishEvent(event, observer);
   } catch (error) {
-    log(error);
+    console.error(error);
   }
 };
