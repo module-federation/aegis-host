3190014040fb67fba4c8f5e5d59c0ec2b313f667
tyson <tyson7757@comcast.net>
2021-04-09 21:44:51 -0500

    docker config

--- src/models/circuit-breaker.js
+++ src/models/circuit-breaker.js
@@ -1,16 +1,72 @@
-// "use strict";
+"use strict";
 
-// const BreakerStatus = {
-//     tripped: [],
-//     closed: [],
-// }
+const THRESHOLD = 5;
+let errorCount = 0;
+let lastError;
 
-// const CurcuitBreaker = function ({ observer, models }) {
-//     function
-//   return {
-//       getStatus(port) {
-//           const keys = Object.keys(BreakerStatus);
-//           keys.filter(key => )
-//       }
-//   };
-// };
+const State = {
+  Open: Symbol(),
+  Closed: Symbol(),
+  HalfOpen: Symbol(),
+};
+
+const Breaker = {
+  state: State.Closed,
+  closed() {
+    return this.state === State.Closed;
+  },
+  open() {
+    return this.state === State.Open;
+  },
+  halfOpen() {
+    return this.state === State.HalfOpen;
+  },
+  trip() {
+    this.state = State.Closed;
+    tripped = Date.now();
+  },
+  test(protectedCall, args) {
+    this.state = State.HalfOpen;
+    try {
+      return protectedCall(...args);
+    } catch (error) {
+      errorCount++;
+    }
+  },
+  reset() {
+    errorCount = 0;
+    this.state = State.Open;
+  },
+};
+
+function thresholdBreached() {
+  const secondsSinceLastError = new Date(Date.now() - lastError).getSeconds();
+  return errorCount > THRESHOLD && secondsSinceLastError < 30;
+}
+
+const Circuit = function (protectedCall) {
+  return {
+    invoke(...args) {
+      if (Breaker.closed()) {
+        try {
+          return protectedCall(...args);
+        } catch (error) {
+          errorCount++;
+          lastError = Date.now();
+          console.error(error, errorCount);
+          return;
+        }
+      }
+
+      if (thresholdBreached()) {
+        Breaker.trip();
+      } else {
+        if (Breaker.test(protectedCall, args)) {
+          Breaker.reset();
+        } else {
+          lastError = Date.now();
+        }
+      }
+    },
+  };
+};
