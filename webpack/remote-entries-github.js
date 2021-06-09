39adaf0e5bd96c8a936ae5ff8aebc8403ea6e7fc
tyson <tyson7757@comcast.net>
2021-06-09 00:07:14 -0500

    cache

--- webpack/remote-entries-github.js
+++ webpack/remote-entries-github.js
@@ -1,21 +1,21 @@
 module.exports = [
   {
     name: "microservices",
-    url: "https://api.github.com/repos/module-federation/MicroLib-Example/contents/dist?ref=customer-server",
+    url: "https://api.github.com/repos/module-federation/MicroLib-Example/contents/dist?ref=master",
     path: __dirname,
     type: "model",
     importRemote: async () => import("microservices/models"),
   },
   {
     name: "adapters",
-    url: "https://api.github.com/repos/module-federation/MicroLib-Example/contents/dist?ref=customer-server",
+    url: "https://api.github.com/repos/module-federation/MicroLib-Example/contents/dist?ref=master",
     path: __dirname,
     type: "adapter",
     importRemote: async () => import("microservices/adapters"),
   },
   {
     name: "services",
-    url: "https://api.github.com/repos/module-federation/MicroLib-Example/contents/dist?ref=customer-server",
+    url: "https://api.github.com/repos/module-federation/MicroLib-Example/contents/dist?ref=master",
     path: __dirname,
     type: "service",
     importRemote: async () => import("microservices/services"),
