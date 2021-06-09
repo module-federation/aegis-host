96315e905a7fccdda712b1156ec5be0aadd6c656
tyson <tyson7757@comcast.net>
2021-06-09 06:48:40 -0500

    fix

--- webpack/remote-entries.js
+++ webpack/remote-entries.js
@@ -1,9 +1,9 @@
-const githubEntries = require("./remote-entries-github");
-// const githubCustomer = require("./remote-entries-github-customer");
+//const githubEntries = require("./remote-entries-github");
+const githubCustomer = require("./remote-entries-github-customer");
 // const githubOrder = require("./remote-entries-github-order");
 // const cacheEntries = require("./remote-entries-cache");
 
-module.exports = githubEntries;
+//module.exports = githubEntries;
 // module.exports = githubEntries.concat(cacheEntries);
-// module.exports = githubCustomer;
+module.exports = githubCustomer;
 // module.exports = githubCustomer.concat(cacheEntries);
