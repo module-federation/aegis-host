0e89b3e5484596e037c83791c688819af19d504d
tyson <tyson7757@comcast.net>
2021-08-09 02:56:03 -0500

    wasm

--- public/wsapp.js
+++ public/wsapp.js
@@ -43,7 +43,7 @@
       ws.close();
     }
 
-    ws = new WebSocket(`ws://webswitch.aegis.dev:8062`);
+    ws = new WebSocket(`ws://${location.hostname}:8062`);
     ws.onerror = function () {
       showMessage("WebSocket error");
     };
