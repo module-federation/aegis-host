02def28b669d24a49ab2dc8a81f8824a443294f8
trmidboe <trmibdoe@gmail.com>
2021-09-02 04:07:04 -0500

    websocket blobs

--- public/wsapp.js
+++ public/wsapp.js
@@ -1,71 +1,86 @@
-(function () {
-  const messages = document.querySelector("#messages");
-  const wsButton = document.querySelector("#wsButton");
-  const statusButton = document.querySelector("#status");
-  const clearButton = document.querySelector("#clear");
+;(function () {
+  const messages = document.querySelector('#messages')
+  const wsButton = document.querySelector('#wsButton')
+  const statusButton = document.querySelector('#status')
+  const clearButton = document.querySelector('#clear')
 
-  function prettifyJson(json) {
-    if (typeof json !== "string") {
-      json = JSON.stringify(json, null, 2);
+  function prettifyJson (json) {
+    if (typeof json !== 'string') {
+      json = JSON.stringify(json, null, 2)
     }
     return json.replace(
       /("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g,
       function (match) {
-        let cls = "<span>";
+        let cls = '<span>'
         if (/^"/.test(match)) {
           if (/:$/.test(match)) {
-            cls = "<span class='text-warning'>";
+            cls = "<span class='text-warning'>"
           } else {
-            cls = "<span>";
+            cls = '<span>'
           }
         } else if (/true|false/.test(match)) {
-          cls = "<span class='text-white'>";
+          cls = "<span class='text-white'>"
         } else if (/null/.test(match)) {
-          cls = "<span class='text-info'>";
+          cls = "<span class='text-info'>"
         }
-        return cls + match + "</span>";
+        return cls + match + '</span>'
       }
-    );
+    )
   }
 
-  function showMessage(message) {
-    document.getElementById("jsonCode").innerHTML += `\n${prettifyJson(
+  function showMessage (message) {
+    document.getElementById('jsonCode').innerHTML += `\n${prettifyJson(
       message
-    )}`;
-    messages.scrollTop = messages.scrollHeight;
+    )}`
+    messages.scrollTop = messages.scrollHeight
   }
 
-  let ws;
+  let ws
 
   wsButton.onclick = function () {
     if (ws) {
-      ws.onerror = ws.onopen = ws.onclose = null;
-      ws.close();
+      ws.onerror = ws.onopen = ws.onclose = null
+      ws.close()
     }
 
-    ws = new WebSocket(`ws://${location.hostname}:${location.port}`);
+    ws = new WebSocket(`ws://${location.hostname}:${location.port}`)
     ws.onerror = function (e) {
-      showMessage("WebSocket error", e);
-    };
+      showMessage('WebSocket error', e)
+    }
     ws.onopen = function () {
-      showMessage("WebSocket connection established");
-      ws.send(JSON.stringify("webswitch"));
-    };
+      showMessage('WebSocket connection established')
+      ws.send(JSON.stringify('webswitch'))
+    }
     ws.onclose = function () {
-      showMessage("WebSocket connection closed");
-      ws = null;
-    };
-    ws.onmessage = function (data) {
-      showMessage(JSON.stringify(JSON.parse(data.data), undefined, 2));
-    };
-  };
+      showMessage('WebSocket connection closed')
+      ws = null
+    }
+    ws.onmessage = function (event) {
+      try {
+        if (event.data instanceof Blob) {
+          reader = new FileReader()
+
+          reader.onload = () => {
+            console.log('Result: ' + reader.result)
+            showMessage(JSON.stringify(JSON.parse(reader.result), undefined, 2))
+          }
+
+          reader.readAsText(event.data)
+        } else {
+          showMessage(JSON.stringify(JSON.parse(event.data), undefined, 2))
+        }
+      } catch (err) {
+        console.error('onmessage', event, err.message)
+      }
+    }
+  }
 
   statusButton.onclick = function () {
-    console.log("sending status");
-    ws.send(JSON.stringify("status"));
-  };
+    console.log('sending status')
+    ws.send(JSON.stringify('status'))
+  }
 
   clearButton.onclick = function () {
-    document.getElementById("jsonCode").innerHTML = "";
-  };
-})();
+    document.getElementById('jsonCode').innerHTML = ''
+  }
+})()
