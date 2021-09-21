4372d89e5cc8ea989a6f2990b0eebba9cda6da2f
trmidboe <trmibdoe@gmail.com>
2021-09-21 05:39:57 -0500

    wasm

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,37 @@
-require("dotenv").config();
-const path = require("path");
-const { ModuleFederationPlugin } = require("webpack").container;
-const nodeExternals = require("webpack-node-externals");
-const fetchRemotes = require("./webpack/fetch-remotes");
-const remoteEntries = require("./webpack/remote-entries");
-const port = process.env.PORT || 8070;
-const sslPort = process.env.SSL_PORT || 8071;
-const sslEnabled = /true/i.test(process.env.SSL_ENABLED);
-const publicPort = sslEnabled ? sslPort : port;
-const chalk = require("chalk");
+require('dotenv').config()
+const path = require('path')
+const { ModuleFederationPlugin } = require('webpack').container
+const nodeExternals = require('webpack-node-externals')
+const fetchRemotes = require('./webpack/fetch-remotes')
+const remoteEntries = require('./webpack/remote-entries')
+const port = process.env.PORT || 8070
+const sslPort = process.env.SSL_PORT || 8071
+const sslEnabled = /true/i.test(process.env.SSL_ENABLED)
+const publicPort = sslEnabled ? sslPort : port
+const chalk = require('chalk')
 
 module.exports = env => {
-  console.log(env);
+  console.log(env)
   if (env.serverless) {
-    remoteEntries.forEach(e => (e.path = "webpack"));
-    console.log(chalk.yellow("serverless build"));
+    remoteEntries.forEach(e => (e.path = 'webpack'))
+    console.log(chalk.yellow('serverless build'))
   }
   return new Promise(resolve => {
     fetchRemotes(remoteEntries).then(remotes => {
-      console.info(remotes);
+      console.info(remotes)
       resolve({
         externals: [nodeExternals()],
-        target: "async-node",
-        mode: "development",
-        devtool: "source-map",
-        entry: ["@babel/polyfill", path.resolve(__dirname, "src/server.js")],
+        target: 'async-node',
+        mode: 'development',
+        devtool: 'source-map',
+        entry: ['@babel/polyfill', path.resolve(__dirname, 'src/server.js')],
         output: {
           publicPath: `http://localhost:${publicPort}`,
-          path: path.resolve(__dirname, "dist"),
-          libraryTarget: "commonjs",
+          path: path.resolve(__dirname, 'dist'),
+          libraryTarget: 'commonjs'
         },
         resolve: {
-          extensions: [".js"],
-          alias: {
-            "@domain": path.resolve(__dirname, "node_modules/@module-federation/aegis/lib/domain"),
-            "@adapters": path.resolve(__dirname, "node_modules/@module-federation/aegis/lib/adapters"),
-            "@services": path.resolve(__dirname, "node_modules/@module-federation/aegis/lib/services"),
-            "@use-cases": path.resolve(__dirname, "node_modules/@module-federation/aegis/lib/use-cases")
-          }
+          extensions: ['.js']
         },
         module: {
           rules: [
@@ -45,32 +39,32 @@ module.exports = env => {
               test: /\.js?$/,
               exclude: /node_modules/,
               use: {
-                loader: "babel-loader",
+                loader: 'babel-loader',
                 options: {
-                  presets: ["@babel/preset-env"],
-                },
-              },
-            },
-          ],
+                  presets: ['@babel/preset-env']
+                }
+              }
+            }
+          ]
         },
         plugins: [
           new ModuleFederationPlugin({
-            name: "microlib",
-            filename: "remoteEntry.js",
+            name: 'microlib',
+            filename: 'remoteEntry.js',
             library: {
-              name: "microlib",
-              type: "commonjs-module",
+              name: 'microlib',
+              type: 'commonjs-module'
             },
-            remoteType: "commonjs-module",
+            remoteType: 'commonjs-module',
             remotes,
             exposes: {
-              "./server": "./src/server",
-              "./domain": "@module-federation/aegis/lib/domain",
-              "./remoteEntries": "./webpack/remote-entries",
-            },
-          }),
-        ],
-      });
-    });
-  });
-};
+              './server': './src/server',
+              './domain': '@module-federation/aegis/lib/domain',
+              './remoteEntries': './webpack/remote-entries'
+            }
+          })
+        ]
+      })
+    })
+  })
+}
