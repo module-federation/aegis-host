73b60a482f275a06e06d172d3117bdb9c6352760
trmidboe <trmibdoe@gmail.com>
2021-11-05 09:55:46 -0500

    proxy

--- proxy.js
+++ proxy.js
@@ -1,6 +1,6 @@
 var proxy = require('express-http-proxy')
 var app = require('express')()
 
-app.use('/', proxy('http://localhost:8070'))
+app.use('/', proxy(process.argv[2]))
 
-app.listen()
+app.listen(process.argv[3])
