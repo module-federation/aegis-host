462c8e3cd37691f480d9742daa62ca38a106fd47
trmidboe <trmibdoe@gmail.com>
2021-11-08 07:12:31 -0600

    ignore port colsn

--- src/index.js
+++ src/index.js
@@ -296,7 +296,7 @@ async function createSecureContext (renewal = false) {
  * to secure port (443) if SSL is enabled.
  */
 function startHttpServer () {
-  if (process.argv[2] === sslPort) return // another instance
+  if (process.argv[2] === sslPort || parseInt(process.argv[2] !== NaN)) return // another instance
   // always run unsecured ports
   const httpServer = http.createServer(app)
   // Use graceful shutdown middleware
