169ab18579923d46dc386bc2e9953c5303e89dbd
trmidboe <trmibdoe@gmail.com>
2021-11-28 23:21:17 -0600

    chmod

--- start-1.sh
+++
@@ -1,6 +0,0 @@
-# Export developer token if using Github as remote (set in user profile)
-export GITHUB_TOKEN=$GITHUB_TOKEN
-# Use sudo since we need to bind to ports 80, 443, run as daemon and log to webroot
-sudo nohup ${NVM_BIN}/node --title aegis dist/index.js >public/aegis.log 2>&1 &
-# display result of command
-./status.sh
