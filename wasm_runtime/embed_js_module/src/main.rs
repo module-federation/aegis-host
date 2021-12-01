a1b250513cde020ccdc452e43afc3a133bd257bb
trmidboe <trmibdoe@gmail.com>
2021-12-01 02:44:09 -0600

    embed aegis in wasm runtime

---
+++ wasm_runtime/embed_js_module/src/main.rs
@@ -0,0 +1,24 @@
+use wasmedge_quickjs::*;
+
+fn main() {
+    let mut ctx = Context::new();
+
+    let code = r#"
+    import('async_demo.js').then((demo)=>{
+        return demo.wait_simple_val(1)
+    })
+    "#;
+
+    let p = ctx.eval_global_str(code);
+    println!("before poll:{:?}", p);
+    if let JsValue::Promise(ref p) = p {
+        let v = p.get_result();
+        println!("v = {:?}", v);
+    }
+    ctx.promise_loop_poll();
+    println!("after poll:{:?}", p);
+    if let JsValue::Promise(ref p) = p {
+        let v = p.get_result();
+        println!("v = {:?}", v);
+    }
+}
