79453012a0dde66d09a9b65fa41c18a880b20b6b
trmidboe <trmibdoe@gmail.com>
2021-12-27 22:34:50 -0600

    refactor

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 # downgrade TLS version - problem with websockets lib
-//export NODE_OPTIONS=--openssl-legacy-provider
+# export NODE_OPTIONS=--openssl-legacy-provider
 # Use sudo since we need to bind to ports 80, 443, run as daemon and log to webroot
-sudo nohup ${NVM_BIN}/node --title aegis dist/index.js >public/aegis.log 2>&1 &
+sudo nohup ${NVM_BIN}/node --title aegis $HOME/aegis-host/dist/index.js >$HOME/aegis-host/public/aegis.log 2>&1 &
 # display result of command
 echo "checking status..."
 sleep 4
-./status.sh
+$HOME/aegis-host/status.sh
