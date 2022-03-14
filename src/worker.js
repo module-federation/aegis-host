243706f09ef36c4d313331edfd8c14ad709ab040
tysonrm <tyson.midboe@gmail.com>
2022-03-14 03:17:07 -0500

    events

--- src/worker.js
+++ src/worker.js
@@ -12,6 +12,7 @@ const { StorageService } = services
 const { initCache } = adapters.controllers
 const { find, save } = StorageAdapter
 const overrides = { find, save, StorageService }
+
 /** @type {import('@module-federation/aegis/lib/domain/event-broker').EventBroker} */
 const broker = EventBrokerFactory.getInstance()
 
@@ -36,24 +37,20 @@ async function init (remotes) {
 /**
  * Create a subchannel between this thread and the main thread that
  * is dedicated to sending and receivng events. Connect each thread-
- * local event broker to the channel by subcribing and publishing to
- * worker-generated events are forwarded to main, except those
- * sent by main, which are to be handled as commands to the thread.
+ * local event {@link broker} to the channel using pub/sub functions
  *
  * @param {MessagePort} eventPort
  */
 function connectEventChannel (eventPort) {
   try {
     // fire external events from main
-    eventPort.onmessage = async msgEvent => {
-      console.debug('worker: from main', msgEvent.data)
+    eventPort.onmessage = async msgEvent =>
       await broker.notify('from_main', msgEvent.data)
-    }
+
     // forward internal events to main
-    broker.on('to_main', event => {
-      console.debug('worker: to main', event)
+    broker.on('to_main', event =>
       eventPort.postMessage(JSON.parse(JSON.stringify(event)))
-    })
+    )
   } catch (error) {
     console.error({ fn: connectEventChannel.name, error })
   }
@@ -77,13 +74,14 @@ remoteEntries.then(remotes => {
       parentPort.on('message', async message => {
         // The message port is transfered
         if (message.eventPort instanceof MessagePort) {
-          const cache = initCache()
-          await cache.load()
+          // load distributed cache
+          await initCache().load()
+          // send/recv events to/from main thread
           connectEventChannel(message.eventPort)
           return
         }
 
-        // Call the use case service named in the message                                                                                                                                                                                                                        q
+        // Call the use case function by `name`                                                                                                                                                                                                                       q
         if (typeof service[message.name] === 'function') {
           const result = await service[message.name](message.data)
           parentPort.postMessage(JSON.parse(JSON.stringify(result)))
