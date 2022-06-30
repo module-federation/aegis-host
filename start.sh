19d2e77096677adc0984764e4a7f7bf3ca9f9621
tyson <tyson.midboe@gmail.com>
2022-06-30 05:23:28 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 # downgrade TLS version - problem with websockets lib
 # export NODE_OPTIONS=--openssl-legacy-provider
 # Use sudo since we need to bind to ports 80, 443, run as daemon and log to webroot
-sudo nohup ${NVM_BIN}/node --title aegis $HOME/aegis-host/dist/main.js >$HOME/aegis-host/public/aegis.log 2>&1 &
+sudo nohup ${NVM_BIN}/node --title aegis $HOME/aegis-host/src/bootstrap.js.js >$HOME/aegis-host/public/aegis.log 2>&1 &
 # display result of command
 echo "checking status..."
 sleep 2
