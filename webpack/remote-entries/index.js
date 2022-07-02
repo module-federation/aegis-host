7b70b820f0199afd32604ee96fdfb533d6ab8f9a
tyson <tyson.midboe@gmail.com>
2022-07-02 10:18:13 -0500

    enable cache

--- webpack/remote-entries/index.js
+++ webpack/remote-entries/index.js
@@ -2,4 +2,6 @@
 // exports.customer = require('./customer')
 // exports.customerLocal = require('./customer-local')
 // exports.order = require('./order')
+
 exports.local = require('./local')
+exports.cache = require('./cache')
