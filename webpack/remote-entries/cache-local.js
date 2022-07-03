655c8cd9491bd91d6d4b9f4be13e65a285edfb8f
tyson <tyson.midboe@gmail.com>
2022-07-03 17:27:17 -0500

    cache

--- webpack/remote-entries/cache-local.js
+++ webpack/remote-entries/cache-local.js
@@ -1,21 +1,21 @@
 exports.cache = [
   {
     name: 'distributed-cache',
-    url: 'http://localhost:8000',
+    url: 'http://localhost:8000/remoteEntry.js',
     path: __dirname,
     type: 'model-cache',
     importRemote: async () => import('distributed-cache/model-cache')
   },
   {
     name: 'adapter-cache',
-    url: 'http://localhost:8000',
+    url: 'http://localhost:8000/remoteEntry.js',
     path: __dirname,
     type: 'adapter-cache',
     importRemote: async () => import('distributed-cache/adapter-cache')
   },
   {
     name: 'service-cache',
-    url: 'http://localhost:8000',
+    url: 'http://localhost:8000/remoteEntry.js',
     path: __dirname,
     type: 'service-cache',
     importRemote: async () => import('distributed-cache/service-cache')
