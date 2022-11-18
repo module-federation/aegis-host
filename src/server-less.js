4fd1cab3cb3a5f55bf5216ac98672ae0ac793001
tysonrm <trmidboe@gmail.com>
2022-11-18 02:33:53 -0600

    refactor serverless entry point

--- src/server-less.js
+++ src/server-less.js
@@ -9,12 +9,12 @@ const remoteEntries = remote.get('./remoteEntries').then(factory => factory())
 
 const adapter = remoteEntries
   .then(remotes => aegis.init(remotes))
-  .then(handle => ServerlessAdapter(handle))
+  .then(handle => ServerlessAdapter()(handle))
 
 /**
  * Serverless entry point. Configure the serverless platform
  * to call this function.
  */
 exports.handleServerless = async function (...args) {
-  return adapter.then(adapter => adapter.handle(...args))
+  return adapter.then(handle => handle(...args))
 }
